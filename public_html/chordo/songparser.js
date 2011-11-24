64b5dc5c2b4573a94f452e9ed9f7cddf8c5342c6
Aaron Whyte <aaron.whyte@gmail.com>
2011-11-24 11:22:48 -0800

    moredo chordo

--- public_html/chordo/songparser.js
+++ public_html/chordo/songparser.js
@@ -52,13 +52,20 @@ SongParser.prototype.parseMeasureBreak = function(text) {
   return null;
 };
 
+SongParser.prototype.tokenize = function(text) {
+  var tokens = text.trim().split(/\s+/g);
+  // Spritting empty-string or whitespace results in one empty token.
+  if (tokens.length == 1 && !tokens.length) return [];
+  return tokens;
+};
+
 /**
  * Parses stuff like
  * open G: //4 o o o /2 //3
  * and returns JSON like
  * {
- *   tokens: ["open", "G"],
- *   strings: ["//3", "/2", "o", "o", "o", "//4"]
+ *   tokens: ["open", "C"],
+ *   strings: ["x", "//3", "/2", "o", "o", "1"]
  * }
  * or null if the text didn't have the right syntax
  * @param {string} text
@@ -66,30 +73,16 @@ SongParser.prototype.parseMeasureBreak = function(text) {
 SongParser.prototype.parseChordDef = function(text) {
   var halves = /^([^:]+):([\s\/12345ox]+)$/g.exec(text);
   if (!halves) return null;
-  // Now it's likely that the author was trying to define a chord.
-
-  var keys = halves[1].split(/\s+/g);
-  var vals = halves[2].split(/\s+/g);
-  if (vals.length < 6) return null; // expect 6 strings
-
-  var tokens = [];
-  for (var i = 0; i < keys.length; i++) {
-    if (keys[i]) {
-      tokens.push(keys[i]);
-    }
-  }
-  if (tokens.length == 0) return null; // need at least one token
-
-  var strings = [];
-  for (var i = 0; i < vals.length; i++) {
-    if (!vals[i]) continue;
-    if (!/^([ox]|([\/12345]*[12345]))$/.test(vals[i])) {
+  // Now it's likely, but not certain,
+  // that the author was trying to define a chord.
+  var tokens = this.tokenize(halves[1]);
+  var strings = this.tokenize(halves[2]);
+  if (strings.length != 6) return null; // expect 6 strings
+  for (var i = 0; i < strings.length; i++) {
+    if (!/^([ox]|([\/12345]*[12345]))$/.test(strings[i])) {
       return null;
     }
-    strings.push(vals[i]);
   }
-  if (strings.length != 6) return null; // expect 6 strings
-
   return {
     'tokens': tokens,
     'strings': strings
@@ -97,8 +90,8 @@ SongParser.prototype.parseChordDef = function(text) {
 };
 
 /**
- * This is the last thing we try, so it never returns null.
- * In other words: Garbage in, lyrics out!
+ * This is the last thing we try, so it must never return null.
+ * If there's no colon, then treat everything as tokens.
  * Returns JSON like
  * {
  *   tokens: ["G", "open"],
@@ -107,5 +100,17 @@ SongParser.prototype.parseChordDef = function(text) {
  * @param {string} text
  */
 SongParser.prototype.parsePlayable = function(text) {
-  // IMPLEMENT ME
+  var colonIndex = text.indexOf(':');
+  var tokensText, lyrics;
+  if (colonIndex == -1) {
+    tokensText = text;
+    lyrics = "";
+  } else {
+    tokensText = text.substr(0, colonIndex);
+    lyrics = text.substr(colonIndex + 1, text.length);
+  }
+  return {
+    'tokens': this.tokenize(tokenText),
+    'lyrics': lyrics
+  };
 };
