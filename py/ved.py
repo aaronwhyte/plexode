c8de6b20b133e784395c89cb0b60478c22312b37
Aaron Whyte <aaron.whyte@gmail.com>
2012-01-20 13:58:05 -0800

    silly HTML tabs for editor
    probably won't even use it

--- py/ved.py
+++ py/ved.py
@@ -11,9 +11,26 @@ def formatVed(vorpJsName, vedJsName):
 </head><body>""")
   h.append(format.navDiv(name, 1))
   h.append(format.mainDiv("""
-ved goes here
+<div
+id="jsonTab" class="tab selected" onclick="selectTab('jsonTab')">json</div><div
+id="editorTab" class="tab unselected" onclick="selectTab('editorTab')">editor</div><div
+id="vorpTab" class="tab unselected" onclick="selectTab('vorpTab')">vorp</div>
+<div style="background:#aaf;padding:1em;"> stuff goes here</div>
 <script src=\"../vorp/""" + vorpJsName + """\"></script>
 <script src=\"""" + vedJsName + """\"></script>
+<script>
+function selectTab(name) {
+  var tabNames = ['jsonTab', 'editorTab', 'vorpTab'];
+  for (var i = 0; i < tabNames.length; i++) {
+    var tab = document.getElementById(tabNames[i]);
+    if (tab.id == name) {
+      tab.className = "tab selected";
+    } else {
+      tab.className = "tab unselected";
+    }
+  }
+}
+</script>
 """))
   h.append(format.footer())
   return ''.join(h)
