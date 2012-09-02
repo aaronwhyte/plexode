c88b973e5abbe0c6c26ada96e6094e79edc79bdc
Aaron Whyte <aaron.whyte@gmail.com>
2012-09-01 21:19:03 -0700

    Ported Level 5
    trivial!

--- public_html/vorp/level5/level.js
+++ public_html/vorp/level5/level.js
@@ -1,17 +1,15 @@
 window['main'] = function() {
-  var b = new LevelBuilder();
-  b.mark(0, 0).markX(1600).markY(500).markX(0).markY(0).wall();
-  b.mark(1600-250, 250).exit('../level6/index.html');
-  var z = b.mark(1000, 0).markY(500).door(true);
-  b.mark(700, 0).dir(Prefab.DOWN).grip(
-      function(held) {
-        z.setClosed(!held);
-      });
-  b.mark(0, 250).dir(Prefab.RIGHT).playerAssembler(true);
-  b.mark(50, 50).block();
-  b.mark(50, 100).block();
-  b.mark(100, 50).block();
-  b.mark(1600-100, 100, 1600-100, 500-100).portals();
-  Vorp.startWithLevelBuilder(
-      b, document.getElementById('canvas'), document.getElementById('flags'));
+  var ed = LevelProg.create();
+  ed.wallStart(0, 0).wallToX(1600).wallToY(500).wallToX(0).wallToY(0);
+  ed.mono(VedType.EXIT, 1600-250, 250, {'url': '../level6/index.html'});
+  ed.link(
+      ed.mono(VedType.GRIP, 700, 50),
+      ed.mono(VedType.DOOR, 1000, 250)
+  );
+  ed.mono(VedType.PLAYER_ASSEMBLER, 50, 250);
+  ed.mono(VedType.BLOCK, 50, 50);
+  ed.mono(VedType.BLOCK, 100, 50);
+  ed.mono(VedType.BLOCK, 50, 100);
+  ed.double(VedType.PORTAL, 1600-100, 100, 1600-100, 500-100);
+  ed.startVorp(document.getElementById('canvas'));
 };
