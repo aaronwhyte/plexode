0c698ee8d58b30df3fa27b25ae2e6a6b7647f8ad
Aaron Whyte <aaron.whyte@gmail.com>
2012-09-16 23:25:21 -0700

    playable VedApp test pages
    but now VedApp leaks timer loops when navigating away from test pages.
    Fix that soon.

--- public_html/ved/vedapp.js
+++ public_html/ved/vedapp.js
@@ -13,28 +13,28 @@ function VedApp(rootNode, stor, opt_testLevelMap) {
  * @type {enum}
  */
 VedApp.Mode = {
-  EDITING: "editing",
-  TESTING: "testing"
+  EDITING: 'editing',
+  TESTING: 'testing'
 };
 
 /**
  * @type {enum}
  */
 VedApp.Params = {
-  MODE: "mode",
-  LEVEL: "level"
+  MODE: 'mode',
+  LEVEL: 'level'
 };
 
 VedApp.prototype.render = function() {
 
   if (!this.listeningToHashChanges) {
-    plex.event.addListener(window, "hashchange", this.getHashChangeListener());
+    plex.event.addListener(window, 'hashchange', this.getHashChangeListener());
     this.listeningToHashChanges = true;
   }
 
   // out-of-dom fragment to prevent rerendering as we go
   // (no idea if that really works)
-  var appDiv = plex.dom.ce("div");
+  var appDiv = plex.dom.ce('div');
 
   var hash = plex.url.getFragment();
   var query = plex.url.decodeQuery(hash);
@@ -49,7 +49,7 @@ VedApp.prototype.render = function() {
   }
 
   // put it all in the DOM
-  this.rootNode.innerHTML = "";
+  this.rootNode.innerHTML = '';
   this.rootNode.appendChild(appDiv);
 };
 
@@ -62,8 +62,8 @@ VedApp.prototype.getHashChangeListener = function() {
 
 VedApp.prototype.renderDirectory = function(appDiv) {
   // nuke button
-  var nukeButton = plex.dom.ce("button", appDiv);
-  plex.dom.ct("Nuke and repopulate", nukeButton);
+  var nukeButton = plex.dom.ce('button', appDiv);
+  plex.dom.ct('Nuke and repopulate', nukeButton);
   var self = this;
   nukeButton.onclick = function() {
     self.nuke();
@@ -71,41 +71,65 @@ VedApp.prototype.renderDirectory = function(appDiv) {
     self.render();
   };
 
-  plex.dom.ce("hr", appDiv);
+  plex.dom.ce('hr', appDiv);
 
   // list of levels
   var levelNames = this.stor.getNames();
   for (var i = 0; i < levelNames.length; i++) {
     var levelName = levelNames[i];
-    var levelDiv = plex.dom.ce("div", appDiv);
-    plex.dom.appendClass(levelDiv, "directoryRow");
+    var levelDiv = plex.dom.ce('div', appDiv);
+    plex.dom.appendClass(levelDiv, 'directoryRow');
 
-    var editLink = plex.dom.ce("a", levelDiv);
-    editLink.href = "#" + plex.url.encodeQuery({
+    var editLink = plex.dom.ce('a', levelDiv);
+    editLink.href = '#' + plex.url.encodeQuery({
       mode: VedApp.Mode.EDITING,
       level: levelName
     });
     plex.dom.ct('edit', editLink);
-    plex.dom.appendClass(editLink, "directoryLink");
+    plex.dom.appendClass(editLink, 'directoryLink');
 
-    var testLink = plex.dom.ce("a", levelDiv);
-    testLink.href = "#" + plex.url.encodeQuery({
+    var testLink = plex.dom.ce('a', levelDiv);
+    testLink.href = '#' + plex.url.encodeQuery({
       mode: VedApp.Mode.TESTING,
       level: levelName
     });
     plex.dom.ct('test', testLink);
-    plex.dom.appendClass(testLink, "directoryLink");
+    plex.dom.appendClass(testLink, 'directoryLink');
 
     plex.dom.ct(levelName, levelDiv);
   }
 };
 
 VedApp.prototype.renderEditing = function(appDiv, levelName) {
-  plex.dom.ct("editing " + levelName, appDiv);
+  plex.dom.ct('editing ' + levelName, appDiv);
 };
 
 VedApp.prototype.renderTesting = function(appDiv, levelName) {
-  plex.dom.ct("testing " + levelName, appDiv);
+  plex.dom.ct('testing ' + levelName, appDiv);
+  plex.dom.ce('br', appDiv);
+  var canvas = plex.dom.ce('canvas', appDiv);
+  canvas.height = 600;
+  canvas.width = 600;
+
+  // get level graf
+  var opStor = new OpStor(this.stor, levelName);
+  var levelOps = opStor.getOpsAfterIndex(-1);
+  var grafModel = new GrafModel();
+  grafModel.applyOps(levelOps);
+
+  // create vorp instance
+  var renderer = new Renderer(canvas, new Camera());
+  var gameClock = new GameClock();
+  var sledgeInvalidator = new SledgeInvalidator();
+  var vorp = Vorp.createVorp(renderer, gameClock, sledgeInvalidator);
+
+  // Use Transformer to populate Vorp with Model.
+  var transformer = new Transformer(vorp, gameClock, sledgeInvalidator);
+  transformer.transformModel(grafModel);
+
+  // Start the game up.
+  // TODO: kill it when we navigate away. Leaking timer loops is baaaaad.
+  vorp.startLoop();
 };
 
 VedApp.prototype.nuke = function() {
