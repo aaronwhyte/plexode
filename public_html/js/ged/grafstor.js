b99ddd7360e3f3612a6789cf282365499cb1ff12
Aaron Whyte <aaron.whyte@gmail.com>
2012-10-18 22:52:20 -0700

    moved LevelEd, skeletonized, planned
    Think I've got a handle on the UI code organization now.

---
+++ public_html/js/ged/grafstor.js
@@ -0,0 +1,10 @@
+//StagedGrafStor(opStor, model):
+//- listen to opstor and update model
+//    - setStagedOps(ops)
+//    - commitStagedOps
+//- pubsub about data changes
+//- undo history op pointers (in-memory per-tab for now)
+
+function StagedGrafStor(opStor) {
+
+}
