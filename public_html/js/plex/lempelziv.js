821ce21461b871645e12703b67c38ef8a200509a
Aaron Whyte <aaron.whyte@gmail.com>
2012-12-17 22:03:51 -0800

    LZ stop-code, and empty-string handling
    plus unit tests

--- public_html/js/plex/lempelziv.js
+++ public_html/js/plex/lempelziv.js
@@ -8,11 +8,16 @@ plex.LempelZiv = function(alphabet) {
   this.alphabet = alphabet;
 };
 
+plex.LempelZiv.STOP = 0;
+
 /**
  * @param {string} str  A string made up only of what's in the alphabet.
  * @return {Array} An array of integers.
  */
 plex.LempelZiv.prototype.encodeToIntegers = function(str) {
+  if (str == '') {
+    return [plex.LempelZiv.STOP];
+  }
   var w = '';
   var result = [];
   var dict = this.createEncodingDictionary();
@@ -23,7 +28,7 @@ plex.LempelZiv.prototype.encodeToIntegers = function(str) {
       w = wc;
     } else {
       result.push(dict.get(w));
-      dict.set(wc, dict.length);
+      dict.set(wc, dict.length + 1);
       w = String(c);
     }
   }
@@ -32,6 +37,7 @@ plex.LempelZiv.prototype.encodeToIntegers = function(str) {
   if (w !== "") {
     result.push(dict.get(w));
   }
+  result.push(plex.LempelZiv.STOP);
   return result;
 };
 
@@ -40,28 +46,37 @@ plex.LempelZiv.prototype.encodeToIntegers = function(str) {
  * @return {string} A string made up only of what's in the alphabet.
  */
 plex.LempelZiv.prototype.decodeFromIntegers = function(ints) {
+  if (ints.length == 1 && ints[0] == plex.LempelZiv.STOP) {
+    return '';
+  }
   var entry = '';
   var dict = this.createDecodingDictionary();
   var w = dict.get(ints[0]);
   var result = w;
 
-  for (var i = 1; i < ints.length; i += 1) {
+  for (var i = 1; i < ints.length; i++) {
     var k = ints[i];
+    if (k == plex.LempelZiv.STOP) {
+      break;
+    }
     if (dict.contains(k)) {
       entry = dict.get(k);
     } else {
-      if (k === dict.length) {
+      if (k === dict.length + 1) {
         entry = w + w.charAt(0);
       } else {
-        return null;
+        throw Error('could not decode integer ' + k);
       }
     }
     result += entry;
 
     // Add w+entry[0] to the dictionary.
-    dict.set(dict.length, w + entry.charAt(0));
+    dict.set(dict.length + 1, w + entry.charAt(0));
 
     w = entry;
+  }
+  if (k != 0) {
+    throw Error('k:' + k + ' but expected stop-code:' + plex.LempelZiv.STOP);
   }
   return result;
 
@@ -70,7 +85,7 @@ plex.LempelZiv.prototype.decodeFromIntegers = function(ints) {
 plex.LempelZiv.prototype.createEncodingDictionary = function() {
   var dict = new plex.Map();
   for (var i = 0; i < this.alphabet.length; i++) {
-    dict.set(this.alphabet.charAt(i), i);
+    dict.set(this.alphabet.charAt(i), i + 1);
   }
   return dict;
 };
@@ -78,7 +93,7 @@ plex.LempelZiv.prototype.createEncodingDictionary = function() {
 plex.LempelZiv.prototype.createDecodingDictionary = function() {
   var dict = new plex.Map();
   for (var i = 0; i < this.alphabet.length; i++) {
-    dict.set(i, this.alphabet.charAt(i));
+    dict.set(i + 1, this.alphabet.charAt(i));
   }
   return dict;
 };
