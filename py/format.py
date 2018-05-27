6d1aebdfa4a490dd5f17ee5d29c1055e82d45aa8
Aaron <aaron.whyte@gmail.com>
2018-05-26 22:13:15 -0700

    removed google analytics
    meh

--- py/format.py
+++ py/format.py
@@ -74,11 +74,12 @@ def table(attribs, rows):
         
 
 def footer():
-    return """<script>
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-7758703-1', 'plexode.com');
-ga('send', 'pageview');
-</script></body></html>"""
+    #<script>
+    #(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    #(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    #m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    #})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    #ga('create', 'UA-7758703-1', 'plexode.com');
+    #ga('send', 'pageview');
+    #</script>
+    return """</body></html>"""
